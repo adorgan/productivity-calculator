22ee07bc9a2d5edfaf1da6e53d709e9678bbf3e2
adorgan <adorgan@gmail.com>
2022-01-25 17:03:39 -0600

    Updated Preferences Updated SettingsActivity

--- app/src/main/java/com/myapp/adorg/simplecalculatorv2/Preferences.java
+++ app/src/main/java/com/myapp/adorg/simplecalculatorv2/Preferences.java
@@ -9,18 +9,12 @@ public class Preferences {
     private static final String PREF_IS_24 = "switch_preference_1";
     private static final String KEY_DELETE = "deleteItem";
     private static final String CHANGE_DATE = "date_changed";
-    private static final String CHANGE_LOG = "change_log";
     private static final String CHANGE_LOG2 = "change_log2";
     private static final String PREF_IS_DARK_MODE = "switch_preference_2";
-    private static final String PREF_DARK_MODE = "dark_mode_key";
 
     static int getPrefProd(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getInt(KEY_PROD, 90);
-    }
-    static int getDarkList(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
-                .getInt(String.valueOf(R.string.dark_mode_key), 1);
     }
     static void setPrefProd(Context context, int productivity) {
         PreferenceManager.getDefaultSharedPreferences(context)
@@ -28,21 +22,9 @@ public class Preferences {
                 .putInt(KEY_PROD, productivity)
                 .apply();
     }
-    public static void set24Hr(Context context, boolean is24Hr) {
-        PreferenceManager.getDefaultSharedPreferences(context)
-                .edit()
-                .putBoolean(PREF_IS_24, is24Hr)
-                .apply();
-    }
     static boolean get24Hr(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(PREF_IS_24, false);
-    }
-    public static void setDarkMode(Context context, boolean isDarkMode) {
-        PreferenceManager.getDefaultSharedPreferences(context)
-                .edit()
-                .putBoolean(PREF_IS_DARK_MODE, isDarkMode)
-                .apply();
     }
     public static boolean getDarkMode(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
@@ -66,16 +48,6 @@ public class Preferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putBoolean(CHANGE_DATE, isChanged)
-                .apply();
-    }
-    static boolean getChangeLogSeen(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
-                .getBoolean(CHANGE_LOG, false);
-    }
-    static void setChangeLogSeen(Context context, boolean isChanged) {
-        PreferenceManager.getDefaultSharedPreferences(context)
-                .edit()
-                .putBoolean(CHANGE_LOG, isChanged)
                 .apply();
     }
     static boolean getChangeLogSeen2(Context context) {
--- app/src/main/java/com/myapp/adorg/simplecalculatorv2/SettingsActivity.java
+++ app/src/main/java/com/myapp/adorg/simplecalculatorv2/SettingsActivity.java
@@ -1,19 +1,10 @@
 package com.myapp.adorg.simplecalculatorv2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
-import androidx.appcompat.app.ActionBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.MenuItem;
-import android.widget.Toast;
-
 public class SettingsActivity extends AppCompatActivity {
-
-    SwitchPreference prefDarkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,54 +19,5 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment(), "settings")
                 .commit();
-
-        /*
-        if (Preferences.getDarkMode(getApplicationContext())) {
-            setTheme(R.style.ActionBarAppThemeDark);
-        } else {
-            setTheme(R.style.ActionBarAppTheme);
-        }
-        super.onCreate(savedInstanceState);
-        setupActionBar();
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new GeneralPreferenceFragment()).commit();
     }
-
-    private void setupActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowTitleEnabled(true);
-        }
-
-
-    }
-
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
-
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_generals);
-            setHasOptionsMenu(true);
-
-
-
-
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), MainActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-        */
-
-    }
-
 }
